5f3606b8ad0d19679d82515991758fc57800ff97
rmlw95 <rmlw1995@yahoo.com>
2019-10-27 15:27:35 -0700

    Updated JobHunter.py; added argdict.txt and FileIO.py

---
+++ JobHunter/FileIO.py
@@ -0,0 +1,41 @@
+# This script creates a SQLite db and does a basic query for it. Some initial code for accessing RateMyProfessor.
+# Zachary Rubin, zrubin@rtc.edu
+# CNA 336 Spring 2018
+# # # ---> Source link: https://github.com/RTCedu/CNA336/blob/master/Spring2018/Sql.py
+
+import sqlite3
+import time
+import os
+import urllib.request
+import json
+sqlite_file = "foo.db"
+conn = sqlite3.connect(sqlite_file)
+c = conn.cursor()
+
+c.execute("CREATE TABLE IF NOT EXISTS Ratings(ID INT PRIMARY KEY AUTOINCREMENT, "
+          "Prof_Name TEXT, Overall_Quality FLOAT, Level_Difficulty FLOAT, Date DATE)")
+#c.execute("INSERT INTO Ratings(Prof_Name, Overall_Quality, Level_Difficulty, Date) "
+#                "VALUES(?, ?, ?, ?)", ("McCormick", 5.0, 4.0, '2016-05-19'))
+# 2016-05-19
+counter = 0
+
+query = "https://data.seattle.gov/api/views/cf52-s8er/rows.json?accessType=DOWNLOAD"
+jsonpage = 0
+try:
+    contents = urllib.request.urlopen(query)
+    response = contents.read()
+    jsonpage = json.loads(response)
+
+except:
+    pass
+    time.sleep(1)
+
+#print(jsonpage)
+data = json.dumps(jsonpage['data'])
+
+for x in data:
+    print(x)
+#conn.commit()
+
+
+conn.close()
--- JobHunter/JobHunter.py
+++ JobHunter/JobHunter.py
@@ -1,15 +1,12 @@
 # This script pulls from a job website and stores positions into a database. If there is a new posting it notifies the user.
 # CNA 330
-# Zachary Rubin, zrubin@rtc.edu
-import mysql.connector
-import sys
-import json
-import urllib.request
-import os
-import time
+# Modified version of code originally written by Zachary Rubin, zrubin@rtc.edu
+import mysql.connector, sys, json, urllib.request, os, time
 
-# Connect to database
-# You may need to edit the connect function based on your local settings.
+# Global variable to assign name of table that will be manipulated:
+table = "jobhunt"
+
+# Connect to database - You may need to edit the connect function based on your local settings.
 def connect_to_sql():
     conn = mysql.connector.connect(user='root', password='',
                                   host='127.0.0.1',
@@ -17,38 +14,40 @@ def connect_to_sql():
     return conn
 
 # Create the table structure
-def create_tables(cursor, table):
-    ## Add your code here. Starter code below
-    cursor.execute('''CREATE TABLE IF NOT EXISTS tablename (id INT PRIMARY KEY); ''')
+def create_tables(cursor, newtable):
+    ## Starter code was here before, more code has now been added
+    cursor.execute(f"CREATE TABLE IF NOT EXISTS {newtable} (id INT PRIMARY KEY AUTO_INCREMENT, type TEXT, url TEXT, created_at TEXT, company_url TEXT, location TEXT, title TEXT, description TEXT, how_to_apply TEXT, company_logo TEXT);")
     return
 
-# Query the database.
-# You should not need to edit anything in this function
+# Query the database. You should not need to edit anything in this function
 def query_sql(cursor, query):
     cursor.execute(query)
     return cursor
 
 # Add a new job
-def add_new_job(cursor, jobdetails):
-    ## Add your code here
-    query = "INSERT INTO"
-    return query_sql(cursor, query)
+def add_new_job(cursor, newtable, jobdetails):
+    ## Starter code was here before, more code has now been added
+    #print(jobdetails['description'])
+    # IGNORE ignores pre-existing values without the need to individually check each potentially redundant value.
+    query = cursor.execute(f"INSERT IGNORE INTO {newtable} (type, url, created_at, company_url, location, title, description, how_to_apply, company_logo) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);", [jobdetails['type'], jobdetails['url'], jobdetails['created_at'], jobdetails['company_url'], jobdetails["location"], jobdetails['title'], jobdetails['description'].encode('unicode_escape'), jobdetails['how_to_apply'],jobdetails['company_logo']]) # jobdetails would be a list of values
+    return cursor #query_sql(cursor, query)
 
 # Check if new job
 def check_if_job_exists(cursor, jobdetails):
-    ## Add your code here
-    query = "SELECT"
+    ## Starter code was here before, more code has now been added
+    query = (f"SELECT {title} FROM {newtable}")
     return query_sql(cursor, query)
 
 def delete_job(cursor, jobdetails):
-    ## Add your code here
-    query = "UPDATE"
+    ## Starter code was here before, more code has now been added
+    query = (f"DELETE FROM {newtable} WHERE title = {title};")
     return query_sql(cursor, query)
 
 # Grab new jobs from a website
 def fetch_new_jobs(arg_dict):
     # Code from https://github.com/RTCedu/CNA336/blob/master/Spring2018/Sql.py
-    query = "https://jobs.github.com/positions.json?" + "location=seattle" ## Add arguments here
+    query = f"https://jobs.github.com/positions.json{arg_dict}"  ## Appends arguments from argdict.txt
+    #print(query)
     jsonpage = 0
     try:
         contents = urllib.request.urlopen(query)
@@ -62,7 +61,7 @@ def fetch_new_jobs(arg_dict):
 def load_config_file(filename):
     argument_dictionary = 0
     # Code from https://github.com/RTCedu/CNA336/blob/master/Spring2018/FileIO.py
-    rel_path = os.path.abspath(os.path.dirname(__file__))
+    rel_path = os.path.abspath(os.path.dirname("FileIO.py"))
     file = 0
     file_contents = 0
     try:
@@ -74,14 +73,18 @@ def load_config_file(filename):
         file.write("")
         file.close()
 
-    ## Add in information for argument dictionary
-    return argument_dictionary
+    ## Added in information for argument dictionary
+    return file_contents #argument_dictionary
 
 # Main area of the code.
-def jobhunt(arg_dict):
+def jobhunt(arg_dict, cursor):
     # Fetch jobs from website
     jobpage = fetch_new_jobs(arg_dict)
-    # print (jobpage)
+    #print(jobpage)                 #debugger
+    #print (json.loads(jobpage))    #debugger
+    for job in jobpage:
+        add_new_job(cursor, table, job)
+
     ## Add your code here to parse the job page
 
     ## Add in your code here to check if the job already exists in the DB
@@ -89,6 +92,7 @@ def jobhunt(arg_dict):
     ## Add in your code here to notify the user of a new posting
 
     ## EXTRA CREDIT: Add your code to delete old entries
+    #cursor.execute(f"DROP TABLE {newtable};")
 
 # Setup portion of the program. Take arguments and set up the script
 # You should not need to edit anything here.
@@ -96,11 +100,14 @@ def main():
     # Connect to SQL and get cursor
     conn = connect_to_sql()
     cursor = conn.cursor()
-    create_tables(cursor, "table")
+    create_tables(cursor, table)
+#   Failed attempt to fix a unicode parsing error because mySQL only supports a maximum of 3-byte characters:
+#    cursor.execute("ALTER DATABASE cna330 CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci';")
+#    conn.commit()      #debugger
     # Load text file and store arguments into dictionary
-    arg_dict = load_config_file(sys.argv[1])
+    arg_dict = load_config_file("argdict.txt")      #modifyed from 'sys.argv[1]' to '"argdict.txt"'
     while(1):
-        jobhunt(arg_dict)
+        jobhunt(arg_dict, cursor)
         time.sleep(3600) # Sleep for 1h
 
 if __name__ == '__main__':
